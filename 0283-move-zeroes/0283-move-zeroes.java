07552f3c157d00f2900bcec3caf095f98a2467da
Tamojeet <89387048+tamojeetK@users.noreply.github.com>
2023-06-23 13:46:37 +0530

    Time: 1 ms (100.00%), Space: 45 MB (55.42%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int idx = 0;
+        for (int i = 0; i < nums.length; i++) {
+
+            // check if the num at i is != 0
+
+            if (nums[i] != 0) {
+
+                nums[idx] = nums[i];
+
+                // check if i and idx are same
+                if (i==idx) {
+                    nums[i] = nums[i];
+                }
+
+                // if not then assign 0 to the no. at i index
+                else {
+                    nums[i] = 0;
+                }
+                idx++;
+            }
+        }
+    }
+}
